c83c684b52a05fb4fbcd4b197486d89b989f9c6f
jesscheng <jesscheng@github.com>
2019-09-25 14:21:28 -0400

    changes to about and contact, update resume

--- contact.php
+++ contact.php
@@ -40,7 +40,7 @@ if(get_magic_quotes_gpc()) {
 // Example $address = "joe.doe@yourdomain.com";
 
 //$address = "example@themeforest.net";
-$address = "jessica.cheng1016@gmail.com";
+$address = "jesca@umich.edu";
 
 
 // Configuration option.
